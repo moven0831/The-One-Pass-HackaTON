0a2583dd05748160a9d85c1f52a49db86a0a3eae
moven0831 <moven0831@gmail.com>
2023-03-15 22:09:17 +0800

    add missing arguments

--- metadata/uploadToIPFS.ts
+++ metadata/uploadToIPFS.ts
@@ -30,7 +30,7 @@ export default async function uploadMetadata(
         apiKey: process.env.MORALIS_WEB3_API_KEY,
     });
 
-    const imageURL = uploadImage(imageName, imagePath);
+    const imageURL = await uploadImage(imageName, imagePath);
     console.log(`imageURL: ${imageURL}`);
     const metadata = {
         name: NFTName,
@@ -52,13 +52,13 @@ export default async function uploadMetadata(
     return response.toJSON()[0].path;
 }
 
-// const testTrait = [{trait_type:"Background",value:"Brown"},{trait_type:"Skin",value:"White"},{trait_type:"Hat",value:"Hat"},{trait_type:"Eye",value:"Blue glasses"},{trait_type:"Diamonds",value:"Green Diamond"}]
+const testTrait = [{trait_type:"Background",value:"Brown"},{trait_type:"Skin",value:"White"},{trait_type:"Hat",value:"Hat"},{trait_type:"Eye",value:"Blue glasses"},{trait_type:"Diamonds",value:"Green Diamond"}]
 
-// uploadMetadata(
-//   "Test_Collection",
-//   "Test_NFT",
-//   "NFTDescription",
-//   testTrait,
-//   "cute_cat.png",
-//   "./imageDir/demo.png"
-// );
+uploadMetadata(
+  "Test_Collection",
+  "Test_NFT",
+  "NFTDescription",
+  testTrait,
+  "cute_cat.png",
+  "./imageDir/demo.png"
+);
